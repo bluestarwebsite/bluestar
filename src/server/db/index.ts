3cf9942005d90451dd5751746d04bfdc7c444df4
ryankuah <ryzkuah@gmail.com>
2024-12-06 11:29:31 +1100

    missed one

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,6 +4,6 @@ import { neon } from "@neondatabase/serverless";
 import { env } from "~/env";
 import * as schema from "./schema";
 
-const sql = neon(env.POSTGRES_URL);
+const sql = neon(env.DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
